ee35ed66fec26638de367c6d565ff2cfc792385e
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-10 18:59:22 +0800

    feat: 添加tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 
 	modules: ["@nuxt/content"],
+	css: ["~/assets/css/main.css"],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
