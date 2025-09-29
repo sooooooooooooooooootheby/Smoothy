afc5815a4ea03a5d8d8108245bcf6f5c0206871d
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-29 16:51:28 +0800

    feat: 添加mdc

--- modules/hello/index.ts
+++ modules/hello/index.ts
@@ -1,4 +1,3 @@
-// modules/my-module/src/module.ts
 import { defineNuxtModule, createResolver, addComponentsDir, addPlugin } from "@nuxt/kit";
 
 export default defineNuxtModule({
@@ -6,13 +5,16 @@ export default defineNuxtModule({
 		name: "my-module",
 		configKey: "myModule",
 	},
-	defaults: {},
-	setup(_, nuxt) {
-		// 解析 runtime 路径（相对于当前文件）
+	defaults: {
+		mdc: true,
+	},
+	setup(options, nuxt) {
+		// 解析 runtime 路径
 		const resolver = createResolver(import.meta.url);
 		const runtimeDir = resolver.resolve("./runtime");
 		const runtimeStylesPath = resolver.resolve("./runtime/styles/content-enhancer.css");
 		const shikiStylesPath = resolver.resolve("./runtime/styles/shiki.css");
+		const componentsPath = resolver.resolve("./runtime/styles/components/index.css");
 
 		// 注册组件
 		addComponentsDir({
@@ -28,5 +30,13 @@ export default defineNuxtModule({
 		nuxt.options.css = nuxt.options.css || [];
 		nuxt.options.css.push(runtimeStylesPath);
 		nuxt.options.css.push(shikiStylesPath);
+		nuxt.options.css.push(componentsPath);
+
+		// 钩子：检查 markdown 文件是否用到 MDC 语法
+		nuxt.hook("pages:extend", (pages) => {
+			if (options.mdc) {
+				nuxt.options.runtimeConfig.public.mdcEnabled = true;
+			}
+		});
 	},
 });
